cbcc90a2142a5911cdf70c6f90c58c06072b4094
Rachel Bloch Mellon <rbmellon@gmail.com>
2018-07-23 14:29:46 -0700

    Add links between various page handlers

--- sample_app/main.py
+++ sample_app/main.py
@@ -5,21 +5,24 @@ import jinja2
 import os
 
 the_jinja_env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPage(webapp2.RequestHandler):
     def get(self): #for a get request
-        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!') #the response
 
-class EnterInfoHandler(webapp2.RequestHandler):
+class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
         welcome_template = the_jinja_env.get_template('templates/welcome.html')
         self.response.write(welcome_template.render())
 
+class GoodbyeHandler(webapp2.RequestHandler):
+    def get(self):
+        goodbye_template = the_jinja_env.get_template('templates/goodbye.html')
+        self.response.write(goodbye_template.render())
+
 app = webapp2.WSGIApplication([
     ('/', MainPage), #this maps the root url to the Main Page Handler
-    ('/enterinfo', EnterInfoHandler)
+    ('/welcome', WelcomeHandler),
+    ('/goodbye', GoodbyeHandler)
 ], debug=True)
