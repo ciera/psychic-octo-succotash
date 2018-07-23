be617273688e06bde1127699e63e69c2f2b7a9b4
Rachel Bloch Mellon <rbmellon@gmail.com>
2018-07-23 15:30:37 -0700

    Rename jinja env to just env.

--- sample_app/main.py
+++ sample_app/main.py
@@ -4,7 +4,7 @@ import webapp2
 import jinja2
 import os
 
-the_jinja_env = jinja2.Environment(
+env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPage(webapp2.RequestHandler):
@@ -13,12 +13,12 @@ class MainPage(webapp2.RequestHandler):
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
-        welcome_template = the_jinja_env.get_template('templates/welcome.html')
+        welcome_template = env.get_template('templates/welcome.html')
         self.response.write(welcome_template.render())
 
 class GoodbyeHandler(webapp2.RequestHandler):
     def get(self):
-        goodbye_template = the_jinja_env.get_template('templates/goodbye.html')
+        goodbye_template = env.get_template('templates/goodbye.html')
         self.response.write(goodbye_template.render())
 
 app = webapp2.WSGIApplication([
